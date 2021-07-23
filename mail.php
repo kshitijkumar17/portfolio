6c8babfd5db3f1fec549292985516305cfb31802
Kshitij Kumar <kshitij1999@gmail.com>
2021-07-23 21:43:07 +0530

    added projects

--- mail.php
+++ mail.php
@@ -2,12 +2,12 @@
 
 $name = $_POST['name'];
 $email= $_POST['email'];
-$subject = $_POST['subject'];
+$subject_mail = $_POST['subject'];
 $message= $_POST['message'];
 
 $to = "kshitij1999@gmail.com";
 $subject = "New Message";
-$txt = "You have a new message:\r\n\r\nName: ". $name . "\r\nEmail: " . $email . "\r\nSubject: " . $subject . "\r\nMessage:" . $message;
+$txt = "You have a new message:\r\n\r\nName: ". $name . "\r\nEmail: " . $email . "\r\nSubject: " . $subject_mail . "\r\nMessage:" . $message;
 
 $headers = "From: noreply@kshitijkumar.tech";
 if($email!=NULL){
